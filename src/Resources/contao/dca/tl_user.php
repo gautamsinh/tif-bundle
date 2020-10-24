d4a39cebeeb1f15658a37b2151c769b9d545d06a
gautamsinh <morigautam13@gmail.com>
2020-10-24 21:25:15 +0530

    Final check

--- src/Resources/contao/dca/tl_user.php
+++ src/Resources/contao/dca/tl_user.php
@@ -1,29 +1,12 @@
 <?php
+
 /*
- * Copyright Gautamsinh Mori <morigautam13@gmail.com>, 2020
- * @author Gautamsinh Mori
- * @link https://github.com/gautamsinh/tif-bundle
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
  */
 
-// contao/dca/tl_user.php
-// Modify the palette
-// $GLOBALS['TL_DCA']['tl_user']['palettes']['default'] = str_replace(
-//     'company',
-//     'loginToken,company',
-//     $GLOBALS['TL_DCA']['tl_user']['palettes']['default']
-// );
+// Extend the default palettes
 
-// Add the field meta data
-// contao/dca/tl_user.php
-
-$GLOBALS['TL_DCA']['tl_user']['fields']['tifToken'] = array(
-    'label' => &$GLOBALS['TL_LANG']['tl_user']['tifToken'],
-    'exclude' => true,
-    'inputType' => 'checkbox',
-    'options' => array('feModules', 'beModules', 'pages', 'articles', 'contents', 'infos'),
-    'reference' => &$GLOBALS['TL_LANG']['tl_user']['tifTokenValues'],
-    'eval' => array('multiple' => true),
-    'sql' => "blob NULL",
-);
