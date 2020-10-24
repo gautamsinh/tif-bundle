f5cf7ba834cdd6aeab88fd010f4f8e109812ca57
gautamsinh <morigautam13@gmail.com>
2020-10-24 21:28:57 +0530

    Final check

--- src/Resources/contao/dca/tl_user.php
+++ src/Resources/contao/dca/tl_user.php
@@ -9,4 +9,29 @@
  */
 
 // Extend the default palettes
+Contao\CoreBundle\DataContainer\PaletteManipulator::create()
+	->addLegend('faq_legend', 'amg_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_BEFORE)
+	->addField(array('faqs', 'faqp'), 'faq_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
+	->applyToPalette('extend', 'tl_user')
+	->applyToPalette('custom', 'tl_user')
+;
 
+// Add fields to tl_user
+$GLOBALS['TL_DCA']['tl_user']['fields']['faqs'] = array
+(
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'foreignKey'              => 'tl_faq_category.title',
+	'eval'                    => array('multiple'=>true),
+	'sql'                     => "blob NULL"
+);
+
+$GLOBALS['TL_DCA']['tl_user']['fields']['faqp'] = array
+(
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'options'                 => array('create', 'delete'),
+	'reference'               => &$GLOBALS['TL_LANG']['MSC'],
+	'eval'                    => array('multiple'=>true),
+	'sql'                     => "blob NULL"
+);
